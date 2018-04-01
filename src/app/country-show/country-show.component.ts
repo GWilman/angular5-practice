df6700e965d4da41717961e888dbc86f214086b6
GWilman <georgewilman1@gmail.com>
2018-04-01 17:26:06 +0100

    added google maps on show page

--- src/app/country-show/country-show.component.ts
+++ src/app/country-show/country-show.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { } from '@types/googlemaps';
 
 import { CountryService } from '../country.service';
 
@@ -9,9 +10,12 @@ import { CountryService } from '../country.service';
   templateUrl: './country-show.component.html',
   styleUrls: ['./country-show.component.css']
 })
+
 export class CountryShowComponent implements OnInit {
 
-  country: {} = {};
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+  country: any = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -29,6 +33,17 @@ export class CountryShowComponent implements OnInit {
     this.countryService.getCountry(code)
         .subscribe(country => {
           this.country = country;
+
+          const mapProp = {
+            center: {
+              lat: this.country.latlng[0],
+              lng: this.country.latlng[1]
+            },
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+          this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
         });
   }
 
